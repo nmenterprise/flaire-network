bdf32d5bfc233f0f854aac21c60a0c4639a76ccd
nmenterprise <102085819+nmenterprise@users.noreply.github.com>
2024-07-20 23:40:14 +0100

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,4 @@
-python3 -m venv venv
-source venv/bin/activate
 pip install alembic
 echo "gotten alembic"
 pip install -r requirements.txt
-python3 manage.py collectstatic --noinput
+python manage.py collectstatic --noinput
