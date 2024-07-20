6aa64077a8459aa9fb74aaeba593b5f6c4758d30
nmenterprise <102085819+nmenterprise@users.noreply.github.com>
2024-07-21 00:02:55 +0100

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,7 @@
+pip3 install django
+pip3 install whitenoise
 pip3 install channels
-pip3 install alembic
-pip3 install binwalk
+pip3 install daphne
 echo "gotten alembic"
 pip3 install -r requirements.txt
 python3 manage.py collectstatic --noinput
