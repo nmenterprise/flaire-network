492fb77bd7af2db38702102037de23a40e227426
nmenterprise <102085819+nmenterprise@users.noreply.github.com>
2024-07-20 22:14:31 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,24 +1,4 @@
-#!/bin/bash
-
-# Update package list and install dependencies
-apt-get update
-apt-get install -y \
-  libdbus-1-dev \
-  cmake \
-  meson \
-  ninja-build \
-  python3-pip
-
-# Install Python dependencies
+python3 -m venv venv
+source venv/bin/activate
 pip install -r requirements.txt
-
-# Build dbus-python
-cd /tmp
-git clone https://github.com/dbus/dbus-python.git
-cd dbus-python
-git checkout 1.3.2  # Ensure you are using the correct version
-meson setup build
-meson compile -C build
-meson install -C build
-
-echo "Build complete!"
+python3 manage.py collectstatic --noinput
