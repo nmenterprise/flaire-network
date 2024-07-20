72843a576edc502c3f8e6eaf45debc7f9b80e1d4
nmenterprise <102085819+nmenterprise@users.noreply.github.com>
2024-07-21 00:05:44 +0100

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -2,5 +2,6 @@ pip3 install django
 pip3 install whitenoise
 pip3 install channels
 pip3 install daphne
+pip3 install python-decouple
 echo "gotten alembic"
 python3 manage.py collectstatic --noinput
