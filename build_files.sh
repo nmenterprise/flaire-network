1cef6c08c93b160eac620526bc7796b321fb3994
nmenterprise <102085819+nmenterprise@users.noreply.github.com>
2024-07-21 00:27:50 +0100

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -3,6 +3,6 @@ pip3 install whitenoise
 pip3 install channels
 pip3 install daphne
 pip3 install python-decouple
-pip3 install psycopg2
+pip3 install psycopg2-binary
 echo "gotten alembic"
 python3 manage.py collectstatic --noinput
