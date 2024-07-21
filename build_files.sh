152022bc029180329c314fc87990e5e527b2f90c
nmenterprise <102085819+nmenterprise@users.noreply.github.com>
2024-07-21 10:47:35 +0100

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -6,3 +6,5 @@ pip3 install python-decouple
 pip3 install psycopg2-binary
 echo "gotten alembic"
 python3 manage.py collectstatic --noinput
+python3 manage.py makemigrations
+python3 manage.py migrate
