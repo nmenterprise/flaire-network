de00a1800264471678510d96a79ff22501e7be80
nmenterprise <102085819+nmenterprise@users.noreply.github.com>
2024-07-21 19:39:23 +0100

    Update asgi.py

--- flare-network/asgi.py
+++ flare-network/asgi.py
@@ -7,7 +7,7 @@ from chat.routing import websocket_urlpatterns
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "https": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
         URLRouter(
             websocket_urlpatterns
