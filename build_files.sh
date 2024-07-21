a03369e8ded8aee5e996f32c7526b2e6ed18ac64
nmenterprise <102085819+nmenterprise@users.noreply.github.com>
2024-07-21 19:28:13 +0100

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -8,7 +8,6 @@ echo "gotten alembic"
 python3 manage.py collectstatic --noinput
 python3 manage.py makemigrations
 python3 manage.py migrate
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('Mr-Ken', 'admin@gmail.com', 'NOvendd7.com')" | python3 manage.py shell
+# echo "from django.contrib.auth.models import User; User.objects.create_superuser('Mr-Ken', 'admin@gmail.com', 'NOvendd7.com')" | python3 manage.py shell
 
 echo "Build process completed successfully."
-daphne -b 0.0.0.0 -p 8000 flare-network.asgi:application
