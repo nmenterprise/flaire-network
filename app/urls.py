c613227b67768c527157662fbbd916ee5530295b
nmenterprise <102085819+nmenterprise@users.noreply.github.com>
2024-07-24 01:21:37 +0100

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import createText
+from .views import createText, register, login_view, home
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('', createText, name='create_textmodel'),
-    # Add other URLs as needed
+    path('', home, name='home'),
+    path('manual/connect/', createText, name='create_textmodel'),
+    path('login/', login_view, name='login_url'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('register/', register, name='register'),
 ]
