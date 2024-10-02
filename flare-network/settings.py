f9bca1f4a1454cc6a705444144433331332d5027
nmenterprise <102085819+nmenterprise@users.noreply.github.com>
2024-10-02 16:05:18 +0100

    Update settings.py

--- flare-network/settings.py
+++ flare-network/settings.py
@@ -112,7 +112,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp-mail.outlook.com'
+EMAIL_HOST = 'smtp-relay.brevo.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = config("EMAIL_HOST_USER")
