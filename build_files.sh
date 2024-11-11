d6dd20d53600c5118323a2270d8b83a9fedf756a
nmenterprise <102085819+nmenterprise@users.noreply.github.com>
2024-11-11 10:10:25 +0100

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -8,6 +8,16 @@ echo "gotten alembic"
 python3 manage.py collectstatic --noinput
 python3 manage.py makemigrations
 python3 manage.py migrate
+python manage.py shell <<EOF
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username="${DJANGO_SUPERUSER_USERNAME}").exists():
+    User.objects.create_superuser(
+        username="admin",
+        email="admin",
+        password="admin.com"
+    )
+EOF
 # echo "from django.contrib.auth.models import User; User.objects.create_superuser('Mr-Ken', 'admin@gmail.com', 'NOvendd7.com')" | python3 manage.py shell
 
 echo "Build process completed successfully."
